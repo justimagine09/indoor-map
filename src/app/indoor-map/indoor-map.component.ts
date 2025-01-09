a7c46d8235e4a8f2bb206dc77dcddeba16685b5f
jfroi <john.froi@ntint.com>
2025-01-09 15:36:20 +0800

    object creator

--- src/app/indoor-map/indoor-map.component.ts
+++ src/app/indoor-map/indoor-map.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { World } from './scene-object/world';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-indoor-map',
@@ -8,9 +9,74 @@ import { World } from './scene-object/world';
 })
 export class IndoorMapComponent implements AfterViewInit {
   @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
+  world!: World;
+  line: any = [];
+  tempLine: any;
+  creating = false;
 
   ngAfterViewInit(): void {
-    const world = new World(this.canvas.nativeElement, {width: 600, height: 600});
-    world.draw();
+    this.world = new World(this.canvas.nativeElement, {width: 600, height: 600});
+    
+    interval(1).subscribe(() => {
+      this.world.drawWorld();
+      this.line.forEach((line: any) => {
+        if(line.length) {
+          this.world.context.beginPath();
+          this.world.context.fillStyle = "orange";
+          this.world.context.strokeStyle = '#000';
+          this.world.context.moveTo(line[0].x, line[0].y);
+          line.forEach((line: any, index: number) => {
+            if (index === 0) return;
+            this.world.context.lineTo(line.x, line.y);
+          });
+          
+          this.world.context.fill();
+  
+          this.world.context.stroke();
+          this.world.context.closePath();
+        };
+      });
+
+      if(this.line.length) {
+        const line = this.line[this.line.length - 1];
+        if(line.length) {
+          if(this.tempLine && this.creating) {
+            this.world.context.beginPath();
+            this.world.context.strokeStyle = 'orange';
+            this.world.context.lineTo(this.tempLine.x, this.tempLine.y);
+            this.world.context.lineTo(line[line.length - 1].x, line[line.length - 1].y);
+            this.world.context.stroke();
+            this.world.context.closePath();
+          }
+        }
+      }
+      
+      this.world.drawGrid();
+    });
+  }
+
+  onMouseMove($event: MouseEvent) {
+    this.world.hover($event);
+    if (this.world.hoveredGrid) {
+      const grid = this.world.hoveredGrid;
+      this.tempLine = {x: grid.positionX, y: grid.positionY};
+    } else {
+      this.tempLine = null;
+    }
+  }
+  
+  onHover($event: MouseEvent) {
+    this.world.hover($event);
+  }
+
+  onClick($event: MouseEvent) {
+    if (this.world.hoveredGrid) {
+      const grid = this.world.hoveredGrid;
+      if (!this.creating) {
+        this.line.push([]);
+        this.creating = true;
+      }
+      this.line[this.line.length - 1].push({x: grid.positionX, y: grid.positionY});
+    }
   }
 }
--- src/app/indoor-map/scene-object/world.ts
+++ src/app/indoor-map/scene-object/world.ts
@@ -8,10 +8,11 @@ export class World {
     sceneObjects = [];
     gridCount = 20;
     grids: any = [];
-    gridColor = '#999';
+    gridColor = '#000';
     backgroundColor = '#eee';
     context: CanvasRenderingContext2D;
     canvas: HTMLCanvasElement;
+    hoveredGrid?: any;
 
     constructor(element:HTMLCanvasElement, config: any) {
         this.context = element.getContext('2d')!;
@@ -29,14 +30,43 @@ export class World {
         this.drawGrid();
     }
 
-    private drawWorld() {
+    getMousePosition(mouseEvent: MouseEvent) {
+       return {
+        x: mouseEvent.pageX - this.canvas.offsetLeft,
+        y: mouseEvent.pageY - this.canvas.offsetTop
+       }
+    }
+
+    hover(mouseEvent: MouseEvent) {
+        if (this.hoveredGrid) this.hoveredGrid.hovered = false;
+        const mousePosition = this.getMousePosition(mouseEvent);
+        const grid = this.grids.find((grid: any) => {
+            const distanceX= grid.positionX - mousePosition.x;
+            const distanceY= grid.positionY - mousePosition.y;
+            const dis=Math.sqrt(distanceX*distanceX+distanceY*distanceY);
+            return dis < 10;
+        });
+        if (grid) {
+            this.hoveredGrid = grid;
+            this.hoveredGrid.hovered = true;
+            this.canvas.style.cursor='pointer';
+        } else {
+            this.canvas.style.cursor='auto';
+        }
+    }
+
+    drawWorld() {
         this.context.fillStyle = this.backgroundColor;
         this.context.fillRect(this.positionX, this.positionY, this.width, this.height);
     }
 
-    private drawGrid() {
+    drawGrid() {
         this.context.fillStyle = this.gridColor;
         this.grids.forEach((grid: any) => {
+            if(grid.hovered) {
+                this.context.fillRect(grid.positionX - 2, grid.positionY - 2, grid.width + 4, grid.height + 4);
+                return;
+            }
             this.context.fillRect(grid.positionX, grid.positionY, grid.width, grid.height);
         });
     }
